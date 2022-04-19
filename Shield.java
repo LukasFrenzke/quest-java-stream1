a0518fe682c0301fa7a1cd904e1fc62f087caddf
LukasFrenzke <lukas.frenzke@telekom.de>
2022-04-19 10:02:31 +0200

    Code added

--- Shield.java
+++ Shield.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Shield {
@@ -17,16 +16,19 @@ public class Shield {
         heroes.add(new Hero("Hulk", 49, true));
         heroes.add(new Hero("Doctor Strange", 42, false));
 
-        List<Hero> elders;
+        List<Hero> elders = heroes.stream()
         // TODO 1 : filter heroes in order to found heroes older than 59
-
+                            .filter(age -> age.getAge() > 59)
+                            .collect(Collectors.toList());
         System.out.println("\nElders:");
         for (Hero elder : elders) {
             System.out.println(elder.getName());
         }
 
-        List<Hero> intolerants;
+        List<Hero> intolerants = heroes.stream()
         // TODO 2 : filter heroes in order to found heroes that are gluten intolerants
+                            .filter(intolerant -> intolerant.isGlutenIntolerant() == true)
+                            .collect(Collectors.toList());
 
         System.out.println("\nGluten intolerants:");
         for (Hero intolerant : intolerants) {
